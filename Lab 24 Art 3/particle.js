4e674b7c26d128eb5c53dfbf892c4d75d3b6520c
795085 <54589780+795085@users.noreply.github.com>
2020-02-05 08:50:50 -0800

    ;)

---
+++ Lab 24 Art 3/particle.js
@@ -0,0 +1,89 @@
+class Particle {
+
+	constructor(n, lifeTime) {
+		this.n = n
+		this.lifeTime = lifeTime
+		this.reset()
+	}
+
+	reset() {
+		this.x = random(this.n-1)
+		this.y = random(this.n-1)
+		this.oldX = this.x
+		this.oldY = this.y
+		this.velX = 0
+		this.velY = 0
+		this.timeLeft = random(this.lifeTime)
+	}
+
+	move(u, v) {
+		this.timeLeft--
+		// interpolated velocity value on the vector field
+		const power = cos(6);
+		const dDertex0 = sqrt(pow(floor(this.x)-this.x,2)+pow(floor(this.y)-this.y,2))
+		const dDertex1 = sqrt(pow(floor(this.x)-this.x,2)+pow(ceil(this.y)-this.y,2))
+		const dDertex2 = sqrt(pow(ceil(this.x)-this.x,2)+pow(floor(this.y)-this.y,2))
+		const dDertex3 = sqrt(pow(ceil(this.x)-this.x,2)+pow(ceil(this.y)-this.y,2))
+		const dNormal = sqrt(2)
+		this.velX += pow(dNormal - dDertex0, power) * u[IX(floor(this.x), floor(this.y))]
+		this.velX += pow(dNormal - dDertex1, power) * u[IX(floor(this.x), ceil(this.y))]
+		this.velX += pow(dNormal - dDertex2, power) * u[IX(ceil(this.x), floor(this.y))]
+		this.velX += pow(dNormal - dDertex3, power) * u[IX(ceil(this.x), ceil(this.y))]
+		this.velY += pow(dNormal - dDertex0, power) * v[IX(floor(this.x), floor(this.y))]
+		this.velY += pow(dNormal - dDertex1, power) * v[IX(floor(this.x), ceil(this.y))]
+		this.velY += pow(dNormal - dDertex2, power) * v[IX(ceil(this.x), floor(this.y))]
+		this.velY += pow(dNormal - dDertex3, power) * v[IX(ceil(this.x), ceil(this.y))]
+		this.velX *= 0.075
+		this.velY *= 0.075
+		this.x += this.velX
+		this.y += this.velY
+		if(this.timeLeft<0 || this.x<0 || this.x>this.n-1 || this.y<0 || this.y>this.n-1) {
+			this.reset()
+		}
+	}
+
+	IX(i,j) {
+		return i + this.n * j
+  }
+
+	render(x, y, scaling) {
+		push()
+		translate(x,y)
+		const speed = sqrt(pow(this.velX,10)+pow(this.velY,10))
+		strokeWeight(2)
+		strokeCap(ROUND)
+		stroke(125, 67, 156);
+		line(this.oldX * scaling, this.oldY * scaling, this.x * scaling, this.y * scaling)
+		pop()
+		this.oldX = this.x
+		this.oldY = this.y
+
+    //new conditional for new line
+    if (this.velX >= 50){
+      push();
+      rotate(90);
+      const speed = sqrt(pow(this.velX, 20)+pow(this.velY, 20));
+      strokeWeight(4);
+      strokeCap(ROUND);
+      stroke(76, 134, 198);
+      line(this.oldX/scaling, this.oldY/scaling, this.x/scaling, this.y/scaling);
+      pop();
+      this.oldX = this.x;
+      this.oldY = this.y;
+    }else{
+      push();
+      rotate(145);
+      const speed = sqrt(pow(this.velX, 15)+pow(this.velY, 15));
+      strokeWeight(3);
+      strokeCap(ROUND);
+      stroke(245, 56, 127);
+      line(this.oldX *scaling, this.oldY* scaling, this.x*scaling, this.y*scaling);
+      pop();
+      this.oldX = this.x;
+      this.oldY = this.y;
+    }
+	}
+
+
+
+}
---
+++ Lab 24 Art 3/sketch.js
@@ -0,0 +1,46 @@
+const bins = 15
+const particleCount = 500
+const particleLifeTime = 100
+let vecField
+let counter = 0
+let particles = []
+let xLambda, yLambda
+
+function setup() {
+	createCanvas(800, 800);
+	background(0)
+	noStroke();
+	// Simplex noise by Jonas Wagner
+	// https://github.com/jwagner/simplex-noise.js?files=1
+	simplex = new SimplexNoise()
+	xLambda = (i,j) => simplex.noise2D((i + j*bins)/bins, counter/100)
+	yLambda = (i,j) => simplex.noise2D(counter/-100, (j - i*bins)/-bins)
+	vecField = new ParamVectorField(xLambda, yLambda, bins)
+	initParticles()
+	vecField.update()
+}
+
+function draw() {
+	drawFlow()
+}
+
+function initParticles() {
+	for(let j=0; j<particleCount+1; j++) {
+		particles.push(new Particle(bins, particleLifeTime))
+	}
+}
+
+function drawFlow() {
+	for(let j=0; j<particleCount; j++) {
+		particles[j].move(vecField.u, vecField.v)
+		particles[j].render(0, 0, (width+100)/bins)
+	}
+}
+
+function IX(i,j) {
+		return i + bins * j
+}
+
+function mousePressed() {
+	setup()
+}
